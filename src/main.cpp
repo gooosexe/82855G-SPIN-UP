538bee8972622851d07048348305079fc91ff172
Oliver <45010534+feelingboredexe@users.noreply.github.com>
2023-01-27 21:20:15 -0500

    shooting works, changed all ports

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,28 @@
 #include <math.h>
 #include <string>
 
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+
+// motors
+pros::Motor mtr_lf(17);
+pros::Motor mtr_lb(16);
+pros::Motor mtr_rf(19);
+pros::Motor mtr_rb(20);
+
+pros::Motor rollerMtr(10);
+//pros::Motor expansionMtr(1);
+
+pros::Motor mtr_intake(9);
+pros::Motor flywheel(8);
+pros::Motor mtr_indexer(7);
+
+// pneumatics+
+pros::ADIDigitalOut pn_indexer ('C');
+pros::ADIDigitalOut pn_expand ('A');
+//pros::ADIDigital
+//pros::ADIDigitalOb ut pn_expand2 ('B');
+
+
 /**
  * A callback function for LLEMU's center button.
  *
@@ -45,8 +67,14 @@ void competition_initialize() {}
 
 void autonomous() {
 
-	
+	mtr_lf.move_relative(-200, 80);
+	mtr_lb.move_relative(-200, 80);
+	mtr_rf.move_relative(200, 80);
+	mtr_rb.move_relative(200, 80);
 
+	pros::delay(1000);
+	
+	rollerMtr.move_relative(-500, -127);
 }
 
 /**
@@ -62,23 +90,6 @@ void autonomous() {
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
-pros::Controller master(pros::E_CONTROLLER_MASTER);
-
-// motors
-pros::Motor mtr_lf(17);
-pros::Motor mtr_lb(9);
-pros::Motor mtr_rf(16);
-pros::Motor mtr_rb(20);
-
-pros::Motor rollerMtr(15);
-pros::Motor expansionMtr(1);
-
-pros::Motor mtr_intake(100);
-
-// pneumatics
-pros::ADIDigitalOut pn_indexer ('C');
-pros::ADIDigitalOut pn_expand1 ('A');
-pros::ADIDigitalOut pn_expand2 ('B');
 
 double strafeAngle; // stores the angle the left stick is pointing
 
@@ -87,89 +98,65 @@ void opcontrol() {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
+
 		double ymotion = master.get_analog(ANALOG_LEFT_Y);
 		double xmotion = master.get_analog(ANALOG_LEFT_X);
 		double rotation = master.get_analog(ANALOG_RIGHT_X);
-		//double roller = master.get_digital(DIGITAL_R1);
-		//double expansion = master.get_digital(DIGITAL_L1);
 		
 		// if roller pressed
 		if (master.get_digital(DIGITAL_L1)) {
-			rollerMtr = -80;
+			rollerMtr = -127;
 		} else {
 			rollerMtr = 0;
 		}
 
-		// if expand pressed
-		if (master.get_digital(DIGITAL_A)) {
-			pn_expand1.set_value(1);
-			pn_expand2.set_value(1);
+		//flywheel
+		if (master.get_digital(DIGITAL_L2)) {
+			flywheel = 127;
 		} else {
-			pn_expand1.set_value(0);
-			pn_expand2.set_value(0);
+			flywheel = 0;
+		}
+
+		// if expand pressed
+		if (master.get_digital(DIGITAL_B)) {
+			pn_expand.set_value(1);
+		} else {
+			pn_expand.set_value(0);
 		}
 		
-		// indexer
+		//indexer
 		if (master.get_digital(DIGITAL_R2)) {
-			pn_indexer.set_value(1);
-			pros::delay(100);
-			pn_indexer.set_value(0);
+			mtr_indexer = -127;
+		} else {
+			mtr_indexer = 0;
 		}
 
 		// intake
 		if (master.get_digital(DIGITAL_R1)) {
-			mtr_intake = 127;
+			mtr_intake = -127;
 		} else {
 			mtr_intake = 0;
 		}
 
 		//drive
-		strafeAngle = atan2(ymotion, xmotion)*180/M_PI; // find angle (only used in the second snipped)
+		//strafeAngle = atan2(ymotion, xmotion)*180/M_PI; // find angle (only used in the second snipped)
 
-		// calculating rotation values and strafe values for each motor (i might have to change the rotation later)
-		int LF = (ymotion - xmotion + rotation);
-		int RF = -(ymotion + xmotion - rotation);
-		int LB = (ymotion + xmotion + rotation);
-		int RB = -(ymotion - xmotion - rotation);
-
-    	// normalize the powers of the motors so that no value exceeds 127
-		int maxPower = abs(LF);
-		if (abs(RF) > maxPower) maxPower = abs(RF);
-		if (abs(LB) > maxPower) maxPower = abs(LB);
-		if (abs(RB) > maxPower) maxPower = abs(RB);
-		if (maxPower > 127) {
-			LF = LF * 127 / maxPower;
-			RF = RF * 127 / maxPower;
-			LB = LB * 127 / maxPower;
-			RB = RB * 127 / maxPower;
-		}
+		int LF = -(ymotion + rotation);
+		int RF = (ymotion - rotation);
+		int LB = -(ymotion + rotation);
+		int RB = (ymotion - rotation);
 
 		mtr_lf = LF;
 		mtr_rf = RF;
 		mtr_lb = LB;
 		mtr_rb = RB;
-
-		/* 	this also works as a replacement for the code above
-		   	however the code above is what chatGPT generated so im going to keep it
-		mtr_lf = -(cos((strafeAngle - 45)*(M_PI/180)) * sqrt(pow(ymotion, 2) + pow(xmotion, 2)));
-		mtr_rb = cos((strafeAngle - 45)*(M_PI/180)) * sqrt(pow(ymotion, 2) + pow(xmotion, 2));
-
-		mtr_rf = sin((strafeAngle - 45)*(M_PI/180)) * sqrt(pow(ymotion, 2) + pow(xmotion, 2));
-		mtr_lb = -(sin((strafeAngle - 45)*(M_PI/180)) * sqrt(pow(ymotion, 2) + pow(xmotion, 2))); 
-		*/
-
-		/* old code for tank drive
-		left_mtr1 = left - turn;
-		left_mtr2 = left - turn;
-		right_mtr1 = -right - turn;
-		right_mtr2 = -right - turn;
-		*/
 		
 		pros::lcd::set_text(0, "Left stick X: " + std::to_string(xmotion));
 		pros::lcd::set_text(1, "Left stick Y: " + std::to_string(ymotion));
 		pros::lcd::set_text(2, "LF power : " + std::to_string(mtr_lf.get_power()) + "\t RB power: "+ std::to_string(mtr_rb.get_power()));
 		pros::lcd::set_text(3, "LB power: " + std::to_string(mtr_lb.get_power()) + "\t RF power: "+ std::to_string(mtr_rf.get_power()));
 		pros::lcd::set_text(4, "Strafe angle: " + std::to_string(strafeAngle));
+		pros::lcd::set_text(5, "Flywheel speed: " + std::to_string(flywheel.get_actual_velocity()));
 
 		pros::delay(20);
 	}
